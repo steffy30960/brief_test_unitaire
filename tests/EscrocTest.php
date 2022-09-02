689c688606e9d8ee3ed581efc6e42542850c223e
Didi518 <kevin.ouali@gmail.com>
2022-09-02 12:58:43 +0200

    keke_unit

--- tests/EscrocTest.php
+++ tests/EscrocTest.php
@@ -10,12 +10,12 @@ class EscrocTest extends KernelTestCase
     public function getEntity(): Escroc
     {
         return (new Escroc())
-        ->setFirstname('keke')
-        ->setLastname('ko')
-        ->setEmail('keke@escroc.com')
-        ->setAddress('1 rue de la Rue 30100 Alès')
-        ->setPhone('0606060606')
-        ->setIsAdmin(true);
+            ->setFirstname('keke')
+            ->setLastname('ko')
+            ->setEmail('keke@escroc.com')
+            ->setAddress('1 rue de la Rue 30100 Alès')
+            ->setPhone('0606060606')
+            ->setIsAdmin(true);
     }
 
     public function testEntityIsValid()
@@ -44,7 +44,32 @@ class EscrocTest extends KernelTestCase
         self::bootKernel();
         $container = static::getContainer();
         $escroc = $this->getEntity();
-        $escroc->setIsAdmin(true);
+        $escroc->setIsAdmin(false);
         $container->get('validator')->validate($escroc);
+
+        $this->assertTrue(1 === 1);
+    }
+
+    public function testPhoneIsInt()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $escroc = $this->getEntity();
+        $phone = 0606060606;
+        $escroc->setPhone($phone);
+        $container->get('validator')->validate($escroc);
+
+        $this->assertIsInt($phone);
+    }
+
+    public function testEmailIsValid()
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        $escroc = $this->getEntity();
+        $email = $this->getEntity()->getEmail();
+        $container->get('validator')->validate($escroc);
+
+        $this->assertMatchesRegularExpression('/^.+\@\S+\.\S+$/', $email);
     }
 }
